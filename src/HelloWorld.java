2cd80c08c66e1a8be1d4136bca36cc24a4d79147
Antonio <atomas@elpuig.xeill.net>
2021-02-01 23:09:48 +0100

    Práctica 1 - E2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Antonio!");
+        System.out.println("Hello Fer!");
         System.out.println("Hello There!");
     }
 }
